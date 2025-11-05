f95a020c6a9d8c5b31bae9c5d219f6cf7484284c
M.V.B.N SREEGANESH <163726114+klu2300031823@users.noreply.github.com>
2025-11-05 10:20:45 +0530

    Update Signin.jsx

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useOutletContext } from 'react-router-dom';
 import './Signin.css';
 import { callApi, setSession } from './api';
 
-const base_url = "http://54.145.40.103:8085"; // Change this when deploying
+const base_url = "http://54.226.198.179:8085"; // Change this when deploying
 
 export default function Signin() {
   const navigate = useNavigate();
